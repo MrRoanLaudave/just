e444b2e8724fa4df61ccdd6d021bf9bf7fb05a9e
MR ROAN LAUDAVE <mrroanlaudave@gmail.com>
2026-08-18 10:27:09 +0100

    Update and rename 4.js to DsABubbleSort.js

---
+++ DsABubbleSort.js
@@ -0,0 +1,18 @@
+const myArray = [7, 3, 9, 12, 11];
+
+const n = myArray.length;
+for (let i = 0; i < n - 1; i++) {
+    let swapped = false;
+    for (let j = 0; j < n - i - 1; j++) {
+        if (myArray[j] > myArray[j + 1]) {
+            // Swap elements using array destructuring
+            [myArray[j], myArray[j + 1]] = [myArray[j + 1], myArray[j]];
+            swapped = true;
+        }
+    }
+    if (!swapped) {
+        break;
+    }
+}
+
+console.log("Sorted array:", myArray);
